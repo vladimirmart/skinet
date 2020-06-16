62c9b165eca4011d06f3580e647e1b16ceb3984b
vladimir.martell@gmail.com <vladimir.martell@gmail.com>
2020-06-15 21:25:36 -0400

    End of Section 3

--- Infrastucture/Data/StoreContext.cs
+++ Infrastucture/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,8 +11,14 @@ namespace Infrastucture.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+            modelbuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
         public DbSet<Product> Products{ get; set; }
-
-
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
     }
 }
