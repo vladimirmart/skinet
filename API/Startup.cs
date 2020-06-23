31cf99b6e6c907444e6c52631f5c8b0d817a8d19
vladimir.martell@gmail.com <vladimir.martell@gmail.com>
2020-06-22 23:11:53 -0400

    End of Section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,13 +1,13 @@
+using API.Extensions;
 using API.Helper;
+using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastucture.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -18,29 +18,24 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-
         }
-
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddAutoMapper(typeof(MappingProfiles));
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
             x.UseSqlite((_config.GetConnectionString("DefaultConnection"))));
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
@@ -49,6 +44,8 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
